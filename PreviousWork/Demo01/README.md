2536e8a4663062032b63044bafe0d3af0ffef18a
cdperez <cdperez.85@gmail.com>
2023-04-22 09:27:56 +0200

    Initial test in React

--- PreviousWork/Demo01/README.md
+++ PreviousWork/Demo01/README.md
@@ -2,4 +2,3 @@
 ## Carlos Díaz Pérez
 
 El propósito de este repositorio es almacenar ejemplos de diferentes lenguajes y librerias para una posible aplición de TDD orientado al workflow existente en [YBVR](https://www.ybvr.com).
-
--- PreviousWork/Demo01/src/App.test.js
+++ PreviousWork/Demo01/src/App.test.js
@@ -3,10 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('link', {name: /learn react/i});
   expect(linkElement).toBeInTheDocument();
 });
 
-test('esto es una pruebba', () => {
-  throw new Error('que dise')
-})
+// test('esto es una pruebba', () => {
+//   throw new Error('que dise')
+// })
